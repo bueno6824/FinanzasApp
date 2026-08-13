f56cef8a4ea0bd213b062eca46b4a383f4da0562
Miguel Angel Bueno Montelongo <bueno6824@gmail.com>
2026-08-12 19:33:32 -0600

    build: actualizar RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+// RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
 
-    
 
     implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
@@ -89,8 +90,7 @@ dependencies {
 
 
 
-// RecyclerView
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
 
 // Material
     implementation("com.google.android.material:material:1.11.0")
